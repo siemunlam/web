2af354e9fc8050747d84417ae9f8441843e546ca
Mattias E. Mignone <memignone@gmail.com>
2017-05-29 16:04:20 -0300

    Creacion de modelo de reglas

--- src/rules/models.py
+++ src/rules/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Categoria(models.Model):
-	descripcion = models.CharField(verbose_name=u'descripción', max_length=30, unique=True)
+	descripcion = models.CharField(verbose_name=u'descripción', max_length=25, unique=True)
 	prioridad = models.PositiveSmallIntegerField(unique=True)
 	color = models.CharField(max_length=6, unique=True)
 	creado = models.DateTimeField(auto_now_add=True)
@@ -32,7 +32,7 @@ class Ajuste(models.Model):
 
 
 class FactorDeAjuste(models.Model):
-	descripcion = models.CharField(verbose_name=u'descripción', max_length=30, unique=True)
+	descripcion = models.CharField(verbose_name=u'descripción', max_length=50, unique=True)
 
 	def __str__(self):
 		return self.descripcion
@@ -43,12 +43,8 @@ class FactorDeAjuste(models.Model):
 		verbose_name_plural = 'factores de ajuste'
 
 
-class Regla(models.Model):
-	pass
-
-
 class FactorDeCategorizacion(models.Model):
-	descripcion = models.CharField(verbose_name=u'descripción', max_length=30, unique=True)
+	descripcion = models.CharField(verbose_name=u'descripción', max_length=50, unique=True)
 
 	def __str__(self):
 		return self.descripcion
@@ -57,3 +53,55 @@ class FactorDeCategorizacion(models.Model):
 		ordering = ['descripcion']
 		verbose_name = 'factor de categorización'
 		verbose_name_plural = 'factores de categorización'
+
+
+class ValorPosibleDeAjuste(models.Model):
+	descripcion = models.CharField(verbose_name=u'descripción', max_length=50, unique=True)
+	factorDeAjuste = models.ForeignKey(FactorDeAjuste)
+
+	def __str__(self):
+		return self.factorDeAjuste.descripcion +" > "+ self.descripcion
+
+	class Meta:
+		ordering = ['descripcion']
+		verbose_name = 'Valor posible de factor de ajuste'
+		verbose_name_plural = 'Valores posibles de factores de ajuste'
+
+
+class ValorPosibleDeCategorizacion(models.Model):
+	descripcion = models.CharField(verbose_name=u'descripción', max_length=50, unique=True)
+	factorDeCategorizacion = models.ForeignKey(FactorDeCategorizacion)
+
+	def __str__(self):
+		return self.factorDeCategorizacion.descripcion +" > "+ self.descripcion
+
+	class Meta:
+		ordering = ['descripcion']
+		verbose_name = 'Valor posible de factor de categorización'
+		verbose_name_plural = 'Valores posibles de factores de categorización'
+
+
+class ReglaDeAjuste(models.Model):
+	condicion = models.ForeignKey(ValorPosibleDeAjuste)
+	resultado = models.ForeignKey(Ajuste)
+
+	def __str__(self):
+		return "Regla "+ self.id +": IF "+ self.condicion +" => AJUSTE:"+ self.resultado
+
+	class Meta:
+		ordering = ['id']
+		verbose_name = 'Regla de ajuste'
+		verbose_name_plural = 'Reglas de ajuste'
+
+
+class ReglaDePreCategorizacion(models.Model):
+	condicion = models.ForeignKey(ValorPosibleDeCategorizacion)
+	resultado = models.ForeignKey(Categoria)
+
+	def __str__(self):
+		return "Regla "+ self.id +": IF "+ self.condicion +" => PRECATEGORIZACION:"+ self.resultado
+
+	class Meta:
+		ordering = ['id']
+		verbose_name = 'Regla de ajuste'
+		verbose_name_plural = 'Reglas de ajuste'
