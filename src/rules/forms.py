d69b5e12120f203e9b6829b0b28a1c5605cc3d18
Mattias Ezequiel Mignone <memignone@gmail.com>
2017-05-31 13:58:42 -0300

    categoriaForm y testCategoriaForm

--- src/rules/forms.py
+++ src/rules/forms.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
+from .models import Categoria
 
 # Create your forms here.
+class CategoriaForm(forms.ModelForm):
+	class Meta:
+		model = Categoria
+		fields = ['descripcion', 'prioridad', 'color']
+		widgets = {
+			'descripcion': forms.TextInput(attrs={'autofocus': True}),
+			'prioridad': forms.NumberInput(attrs={'min': 0})
+		}
---
+++ src/rules/tests/test_forms.py
@@ -0,0 +1,7 @@
+from .. import forms
+
+# Create your test here.
+class TestCategoriaForm():
+    def test_form(self):
+        form = forms.CategoriaForm(data={})
+        assert form.is_valid() is False, 'Should be invalid if no data is submitted'
