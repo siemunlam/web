f81c0b47e549ddc4804bf93910e4a12b9c0d5f47
Mattias Ezequiel Mignone <Mattias Ezequiel Mignone>
2017-06-01 18:36:00 -0300

    creación de todas las Forms

--- src/rules/forms.py
+++ src/rules/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
-from .models import Categoria
+from .models import Categoria, FactorDeAjuste, FactorDePreCategorizacion, ValorDeFactorDeAjuste, ValorDeFactorDePreCategorizacion, ReglaDeAjuste, ReglaDePreCategorizacion
 
 # Create your forms here.
 class CategoriaForm(forms.ModelForm):
@@ -10,5 +10,70 @@ class CategoriaForm(forms.ModelForm):
 		fields = ['descripcion', 'prioridad', 'color']
 		widgets = {
 			'descripcion': forms.TextInput(attrs={'autofocus': True}),
+			'prioridad': forms.NumberInput(attrs={'min': 0})
+		}
+
+
+class FactorDePreCategorizacionForm(forms.ModelForm):
+	class Meta:
+		model = FactorDePreCategorizacion
+		fields = ['descripcion']
+		widgets = {
+			'descripcion': forms.TextInput(attrs={'autofocus': True})
+		}
+
+
+class FactorDeAjusteForm(forms.ModelForm):
+	class Meta:
+		model = FactorDeAjuste
+		fields = ['descripcion']
+		widgets = {
+			'descripcion': forms.TextInput(attrs={'autofocus': True})
+		}
+
+
+class FactorDePreCategorizacionForm(forms.ModelForm):
+	class Meta:
+		model = FactorDePreCategorizacion
+		fields = ['descripcion']
+		widgets = {
+			'descripcion': forms.TextInput(attrs={'autofocus': True})
+		}
+
+
+class ValorDeFactorDeAjusteForm(forms.ModelForm):
+	class Meta:
+		model = ValorDeFactorDeAjuste
+		fields = ['descripcion', 'factorDeAjuste']
+		widgets = {
+			'descripcion': forms.TextInput(attrs={'autofocus': True})
+		}
+
+
+class ValorDeFactorDePreCategorizacionForm(forms.ModelForm):
+	class Meta:
+		model = ValorDeFactorDePreCategorizacion
+		fields = ['descripcion', 'factorDePreCategorizacion']
+		widgets = {
+			'descripcion': forms.TextInput(attrs={'autofocus': True})
+		}
+
+
+class ReglaDeAjusteForm(forms.ModelForm):
+	class Meta:
+		model = ReglaDeAjuste
+		fields = ['condicion', 'resultado', 'prioridad']
+		widgets = {
+			'condicion': forms.Select(attrs={'autofocus': True}),
+			'prioridad': forms.NumberInput(attrs={'min': 0})
+		}
+
+
+class ReglaDePreCategorizacionForm(forms.ModelForm):
+	class Meta:
+		model = ReglaDePreCategorizacion
+		fields = ['condicion', 'resultado', 'prioridad']
+		widgets = {
+			'condicion': forms.Select(attrs={'autofocus': True}),
 			'prioridad': forms.NumberInput(attrs={'min': 0})
 		}
