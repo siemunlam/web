b210bb06321fc36baeb163caa4991536d37d3ff6
fcorrales <federico.corrales.92@gmail.com>
2017-08-02 02:24:18 -0300

    Actualización de labels y formato de fecha
    Se elimina el CamelCase para mensajes de usuarios y se cambia el
    formateo de fecha de toLocateFormat por moment.js porque esta soporta
    Chrome.

--- src/analytics/views.py
+++ src/analytics/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
--- src/siem_web/urls.py
+++ src/siem_web/urls.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """siem_web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
