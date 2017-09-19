49f097167c88ea9b2a0b426976320f2249e3d66a
fcorrales <federico.corrales.92@gmail.com>
2017-09-19 20:47:59 -0300

    Actualización fetch options api get
    Se crea una función genérica para los gets.
    Se agregan options a los fetches que faltaban.

--- static/static/js/utils.js
+++ static/static/js/utils.js
@@ -4,3 +4,16 @@ function checkStatus(response) {
     else
       return Promise.reject(new Error(response.statusText));
   };
+
+function getAuthorizedFetchOption() {
+  const header_init = {
+    'Accept': 'application/json',
+    'X-CSRFToken': getCookie("csrftoken")
+  }
+  fetchOptions = {
+    method: 'GET',
+    headers: new Headers(header_init),
+    credentials: 'same-origin'
+  };
+  return fetchOptions
+};
