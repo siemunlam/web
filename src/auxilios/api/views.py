f6f684525b302538f8d1b2e7c34c6b8d146f3c49
Mattias Ezequiel Mignone <memignone@gmail.com>
2017-09-22 18:12:20 -0300

    Estado actual al suscribirse
    - En el mensaje de éxito de suscripción a un auxilio, devuelve el estado actual del mismo.

--- src/auxilios/api/views.py
+++ src/auxilios/api/views.py
@@ -219,3 +219,12 @@ class SuscriptoresDeAuxilio(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Suscriptor.objects.all()
     serializer_class = SuscriptorDetailSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        responseData = dict(serializer.data)
+        responseData['status'] = Auxilio.objects.get(id=self.kwargs['pk']).estados.first().estado
+        return Response(responseData, status=HTTP_201_CREATED, headers=headers)
