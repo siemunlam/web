1797f341f30f1134b0a7c4078fb96d30523b701e
Mattias Ezequiel Mignone <memignone@gmail.com>
2017-10-14 14:41:38 -0300

    FIX reportes

--- src/auxilios/api/pagination.py
+++ src/auxilios/api/pagination.py
@@ -3,13 +3,14 @@ from rest_framework.response import Response
 from collections import OrderedDict
 
 class CustomPagination(PageNumberPagination):
-    page_size_query_param = 'page_size'
-    def get_paginated_response(self, data):
-        return Response(OrderedDict([
-            ('count', self.page.paginator.count),
-            ('next', self.get_next_link()),
-            ('previous', self.get_previous_link()),
-            ('current_page', self.page.number),
-            ('total_pages', self.page.paginator.num_pages),
-            ('results', data)
-        ]))
+	page_size_query_param = 'page_size'
+	
+	def get_paginated_response(self, data):
+		return Response(OrderedDict([
+			('count', self.page.paginator.count),
+			('next', self.get_next_link()),
+			('previous', self.get_previous_link()),
+			('current_page', self.page.number),
+			('total_pages', self.page.paginator.num_pages),
+			('results', data)
+		]))
